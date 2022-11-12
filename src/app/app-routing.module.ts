7f6fa1c242f193a12309e85edb2b5dd9ad12f6a5
Shlok Chavan <shlok@pentaknot.com>
2022-11-13 00:21:33 +0530

    Test Dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: AuthLayoutComponent,
     loadChildren: () =>
-      import('./modules/landing/landing-routing.module').then(
-        (m) => m.LandingRoutingModule
+      import('./modules/auth/auth.module').then(
+        (m) => m.AuthModule
       ),
   },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+  // {
+  //   path: '',
+  //   loadChildren: () =>
+  //     import('./modules/landing/landing-routing.module').then(
+  //       (m) => m.LandingRoutingModule
+  //     ),
+  // },
 ];
 
 @NgModule({
